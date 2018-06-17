f915759aa70a5e49a3dd7d7ebdfc4d63014beea1
SpaseMilunoski <dan.soto576@gmail.com>
2018-06-17 00:50:48 -0500

    Listo para pruebas

---
+++ MochilaGenetico/Form1.Designer.cs
@@ -0,0 +1,257 @@
+﻿namespace MochilaGenetico
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.lbPruebas = new System.Windows.Forms.ListBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.tbPoblacion = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.tbPC = new System.Windows.Forms.TextBox();
+            this.tbVueltas = new System.Windows.Forms.TextBox();
+            this.label16 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.tbPM = new System.Windows.Forms.TextBox();
+            this.tbDirectorio = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label6 = new System.Windows.Forms.Label();
+            this.lbres = new System.Windows.Forms.Label();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.MediumSpringGreen;
+            this.button1.FlatAppearance.BorderSize = 0;
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button1.Location = new System.Drawing.Point(342, 253);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(101, 30);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Iniciar";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Century Gothic", 16F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(165, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(221, 52);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Algoritmo Genético\r\nMochila";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(248, 70);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(48, 18);
+            this.label5.TabIndex = 16;
+            this.label5.Text = "P(x)= ";
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.lbres);
+            this.groupBox1.Controls.Add(this.lbPruebas);
+            this.groupBox1.Location = new System.Drawing.Point(12, 174);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(288, 154);
+            this.groupBox1.TabIndex = 17;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Resultados";
+            // 
+            // lbPruebas
+            // 
+            this.lbPruebas.FormattingEnabled = true;
+            this.lbPruebas.Location = new System.Drawing.Point(0, 111);
+            this.lbPruebas.Name = "lbPruebas";
+            this.lbPruebas.Size = new System.Drawing.Size(240, 43);
+            this.lbPruebas.TabIndex = 0;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(9, 111);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(118, 13);
+            this.label2.TabIndex = 19;
+            this.label2.Text = "Población (Número par)";
+            // 
+            // tbPoblacion
+            // 
+            this.tbPoblacion.Location = new System.Drawing.Point(12, 127);
+            this.tbPoblacion.Name = "tbPoblacion";
+            this.tbPoblacion.Size = new System.Drawing.Size(100, 20);
+            this.tbPoblacion.TabIndex = 18;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(133, 98);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(110, 26);
+            this.label3.TabIndex = 21;
+            this.label3.Text = "Probabilidad de cruce\r\n(0.65 a 0.80)";
+            // 
+            // tbPC
+            // 
+            this.tbPC.Location = new System.Drawing.Point(136, 127);
+            this.tbPC.Name = "tbPC";
+            this.tbPC.Size = new System.Drawing.Size(100, 20);
+            this.tbPC.TabIndex = 20;
+            // 
+            // tbVueltas
+            // 
+            this.tbVueltas.Location = new System.Drawing.Point(379, 127);
+            this.tbVueltas.Name = "tbVueltas";
+            this.tbVueltas.Size = new System.Drawing.Size(100, 20);
+            this.tbVueltas.TabIndex = 36;
+            // 
+            // label16
+            // 
+            this.label16.AutoSize = true;
+            this.label16.Location = new System.Drawing.Point(388, 101);
+            this.label16.Name = "label16";
+            this.label16.Size = new System.Drawing.Size(77, 13);
+            this.label16.TabIndex = 35;
+            this.label16.Text = "No. de Vueltas";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(248, 101);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(126, 26);
+            this.label4.TabIndex = 34;
+            this.label4.Text = "Probabilidad de mutación\r\n(0.001 a 0.01)";
+            // 
+            // tbPM
+            // 
+            this.tbPM.Location = new System.Drawing.Point(251, 127);
+            this.tbPM.Name = "tbPM";
+            this.tbPM.Size = new System.Drawing.Size(100, 20);
+            this.tbPM.TabIndex = 33;
+            // 
+            // tbDirectorio
+            // 
+            this.tbDirectorio.Location = new System.Drawing.Point(318, 210);
+            this.tbDirectorio.Name = "tbDirectorio";
+            this.tbDirectorio.Size = new System.Drawing.Size(138, 20);
+            this.tbDirectorio.TabIndex = 37;
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.Color.MediumSpringGreen;
+            this.button2.FlatAppearance.BorderSize = 0;
+            this.button2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button2.Location = new System.Drawing.Point(447, 210);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(32, 20);
+            this.button2.TabIndex = 38;
+            this.button2.Text = "...";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(306, 194);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(212, 13);
+            this.label6.TabIndex = 39;
+            this.label6.Text = "Selecione la carpeta que contiene los items";
+            // 
+            // lbres
+            // 
+            this.lbres.AutoSize = true;
+            this.lbres.Location = new System.Drawing.Point(6, 36);
+            this.lbres.Name = "lbres";
+            this.lbres.Size = new System.Drawing.Size(0, 13);
+            this.lbres.TabIndex = 1;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(552, 330);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.tbVueltas);
+            this.Controls.Add(this.label16);
+            this.Controls.Add(this.tbDirectorio);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.tbPM);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.tbPC);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.tbPoblacion);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox tbPoblacion;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox tbPC;
+        private System.Windows.Forms.TextBox tbVueltas;
+        private System.Windows.Forms.Label label16;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox tbPM;
+        private System.Windows.Forms.ListBox lbPruebas;
+        private System.Windows.Forms.TextBox tbDirectorio;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label lbres;
+    }
+}
+
---
+++ MochilaGenetico/Form1.cs
@@ -0,0 +1,370 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace MochilaGenetico
+{
+    public partial class Form1 : Form
+    {        
+        List<Item> Individuos = new List<Item>();
+        List<Item> Padres = new List<Item>();
+        List<double> Fnorm = new List<double>();
+        List<Item> Hijos = new List<Item>();
+        List<double> Acumulado = new List<double>();
+        List<double> itemsPeso = new List<double>();
+        List<double> itemsGanancia = new List<double>();
+        List<String> Mutacion = new List<String>();
+        Random r = new Random();
+        Item mayor = new Item();
+        int Poblacion;
+        double sumatoria = 0;
+        int capacidadMochila;
+        string line;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        public void cargrarDatos()
+        {
+            using (var fd = new FolderBrowserDialog())
+            {
+                if (fd.ShowDialog() == System.Windows.Forms.DialogResult.OK && !string.IsNullOrWhiteSpace(fd.SelectedPath))
+                {
+                    tbDirectorio.Text = fd.SelectedPath;
+                }
+            }
+            llenarDatos(tbDirectorio.Text + @"\c.txt",1);//capacidad de mochila
+            llenarDatos(tbDirectorio.Text + @"\w.txt", 2);//llenado de pesos
+            llenarDatos(tbDirectorio.Text + @"\p.txt", 3);//llenado de ganancias
+        }
+        public void llenarDatos(String directrorio,int v)
+        {
+            try
+            {
+                StreamReader fpeso = new StreamReader(directrorio);
+                while ((line = fpeso.ReadLine()) != null)
+                {
+                    //System.Console.WriteLine(line);
+                    if (v == 1)
+                    {
+                        capacidadMochila = Convert.ToInt32(line);
+                    }
+                    if (v==2)
+                    {
+                        itemsPeso.Add(Convert.ToDouble(line));
+                    }
+                    if (v==3)
+                    {
+                        itemsGanancia.Add(Convert.ToDouble(line));
+                    }
+                    //lbPruebas.Items.Add(line);
+                }
+
+                fpeso.Close();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+        public void inicializacion()
+        {
+            if (itemsPeso.Count == itemsGanancia.Count)
+            {
+                Poblacion = Convert.ToInt32(tbPoblacion.Text);
+                for (int i = 0; i < Poblacion; i++)
+                {
+                    string binario = "";
+                    for (int j = 0; j < itemsPeso.Count; j++)//Generar numero binario aleatorio
+                    {
+                        int random = r.Next(0, 2);
+                        binario = binario + Convert.ToString(random);
+                    }
+                    Item item = new Item();
+                    item.setValor(binario);
+                    Individuos.Add(item);
+                    //lbPruebas.Items.Add(binario);
+                }
+            }
+            else
+            {
+                MessageBox.Show("Error","El numero de items en peso y ganancia no son los mismos");
+            }           
+        }
+        public double Peso(String item)
+        {
+            int cont=0;
+            double peso=0;
+            foreach (char p in item)
+            {
+                if (p=='1')
+                {
+                    peso += itemsPeso[cont];
+                }
+                cont++;
+            }
+            return peso;
+        }
+        public void restricion()
+        {
+            sumatoria = 0;
+            foreach (Item item in Individuos)
+            {
+                //lbPruebas.Items.Add(item.getValor());
+                if (item.getPeso() < capacidadMochila)
+                {
+                    item.setGanancia(funcion(item.getValor()));
+                }
+                else
+                {
+                    item.setGanancia(0);
+                }
+                sumatoria += item.getGanacia();
+            }
+        }
+        public double funcion(String item)
+        {
+            double ganancia=0;
+            int cont = 0;
+            foreach (char p in item)
+            {
+                if (p == '1')
+                {
+                    ganancia += itemsGanancia[cont];
+                }
+                cont++;
+            }
+            return ganancia;
+        }
+        public void fnorm()
+        {
+            int i = 0;
+            double acumulado = 0;
+            foreach(Item item in Individuos)
+            {                                                
+                Fnorm.Add(item.getGanacia() /sumatoria);
+                acumulado = acumulado + Convert.ToDouble(Fnorm[i]);//Calcular acumulado de fnorm
+                Acumulado.Add(acumulado);
+                i++;
+            }
+        }
+        public void limpieza()
+        {
+            lbPruebas.Items.Clear();
+            Individuos.Clear();
+            Padres.Clear();
+            Hijos.Clear();
+            Fnorm.Clear();
+            Acumulado.Clear();
+            Mutacion.Clear();
+        }
+        public void mostrar()
+        {
+            int i = 0;
+           //foreach(Item item in Individuos)
+           // {
+           //     lbPruebas.Items.Add(item.getValor()+"  "+item.getPeso()+"  "+ item.getGanacia()+"  "+Fnorm[i]+"  "+Acumulado[i]);
+           //     i++;
+           // }
+           //foreach(Item hijo in Hijos)
+           // {
+           //     lbPruebas.Items.Add(hijo.getValor());
+           // }
+           //foreach(String mutado in Mutacion)
+           // {
+           //     lbPruebas.Items.Add(mutado);
+           // }
+           foreach(Item indi in Individuos)
+            {
+                lbPruebas.Items.Add(indi.getValor());
+            }
+        }
+        public void ruleta()
+        {
+            calcularMayor();
+            for (int i = 0; i < Poblacion; i++) //seleccion por ruleta
+                {
+                    double random = r.NextDouble();//Numero random entre 0 y 1
+                    for (int j = 0; j < Poblacion; j++)
+                    {
+                        if (Acumulado[j] > random)
+                        {
+                            Padres.Add(Individuos[j]);
+                            j = Poblacion;
+                        }
+                    }
+                }
+        }
+        public void cruce()
+        {
+            String aux1 = "", aux2 = "";
+            double cruce;
+            int pcrt1, pcrt2;
+            for (int i = 0; i < Poblacion - 1; i++)
+            {
+                cruce = r.NextDouble();
+                if (cruce <= Convert.ToDouble(tbPC.Text))
+                {
+                    do
+                    {
+                        pcrt1 = r.Next(0, itemsPeso.Count);//random de 0 a 7
+                        pcrt2 = r.Next(0, itemsPeso.Count);
+                    } while (pcrt1 == pcrt2);
+                    if (pcrt1 < pcrt2)
+                    {
+                        for (int j = 0; j < itemsPeso.Count; j++)
+                        {
+                            if (j >= pcrt1 && j <= pcrt2)
+                            {
+                                aux1 += Padres[i+1].getValor()[j];
+                                aux2 += Padres[i].getValor()[j];
+                            }
+                            else
+                            {
+                                aux1 += Padres[i].getValor()[j];
+                                aux2 += Padres[i + 1].getValor()[j];
+                            }
+
+                        }
+                        Item hijo = new Item();
+                        hijo.setValor(aux1);
+                        Hijos.Add(hijo);
+                        aux1 = "";
+                        hijo = new Item();
+                        hijo.setValor(aux2);
+                        Hijos.Add(hijo);
+                        aux2 = "";
+                    }
+                    else
+                    {
+                        for (int j = 0; j < itemsPeso.Count; j++)
+                        {
+                            if (j <= pcrt2 || j >= pcrt1)
+                            {
+                                aux1 += Padres[i + 1].getValor()[j];
+                                aux2 += Padres[i].getValor()[j];
+                            }
+                            else
+                            {
+                                aux1 += Padres[i].getValor()[j];
+                                aux2 += Padres[i + 1].getValor()[j];
+                            }
+
+                        }
+                        Item hijo = new Item();
+                        hijo.setValor(aux1);
+                        Hijos.Add(hijo);
+                        aux1 = "";
+                        hijo = new Item();
+                        hijo.setValor(aux2);
+                        Hijos.Add(hijo);
+                        aux2 = "";
+                    }
+                    i++;
+                }
+                else
+                {
+                    Hijos.Add(Padres[i]);
+                    i++;
+                    Hijos.Add(Padres[i]);
+                }
+            }
+        }
+        public void mutacion()
+        {
+            for (int i = 0; i < Poblacion; i++) //Recorrer el numero de hijos
+            {
+                string aux = "";
+                for (int j = 0; j < itemsPeso.Count; j++)//Recorrer el numero de elementos de cada hijo
+                {
+                    double al1 = r.NextDouble();
+                    if (al1 < Convert.ToDouble(tbPM.Text))//Si el numero aleatorio es menor que la probabilidad de mutacion
+                    {
+                        if (Hijos[i].getValor()[j] == '0')//si la posicion actual de j en hijo es 0
+                        {
+                            aux += "1";
+                        }
+                        else
+                        {
+                            aux += "0";
+                        }
+                    }
+                    else
+                    {
+                        aux += Hijos[i].getValor().ToString()[j];
+                    }
+                }
+                Mutacion.Add(aux);
+                aux = "";
+            }
+        }
+        public void reinicio()
+        {
+            //lbPruebas.Items.Clear();
+            Individuos.Clear();
+            foreach (String individuo in Mutacion)
+            {
+                Item ind = new Item();
+                ind.setValor(individuo);
+                Individuos.Add(ind);
+            }
+            Padres.Clear();
+            Hijos.Clear();
+            Fnorm.Clear();
+            Acumulado.Clear();
+            Mutacion.Clear();
+        }
+        public void calcularMayor()
+        {
+            foreach(Item individuo in Individuos)
+            {
+                if (mayor.getGanacia()<individuo.getGanacia())
+                {
+                    mayor = individuo;
+                }
+            }
+        }
+        public void genetico()
+        {
+            int vueltas = Convert.ToInt32(tbVueltas.Text);
+            inicializacion();
+            do
+            {
+            foreach(Item item in Individuos)
+            {
+                item.setPeso(Peso(item.getValor()));
+            }
+            
+                restricion();//se aplica la restricion y se calcula la ganancia y la sumatoria
+                fnorm();
+                ruleta();
+                cruce();
+                mutacion();
+                reinicio();
+                mostrar();
+                lbres.Text ="Combinacion de item: "+ mayor.getValor() + "\n Peso: " + mayor.getPeso() + "\n Ganancia: " + mayor.getGanacia();
+                vueltas--;
+            } while (vueltas!=0);
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            limpieza();
+            genetico();
+           
+            
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            cargrarDatos();
+        }
+    }
+}
